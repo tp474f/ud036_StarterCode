60ca3c3349cf8ac21c7fd7c900ad6e979a06be86
tp474f <tp474f@att.com>
2018-08-01 14:57:37 +0000

    [fix] split for both long strings to match standards

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,12 +3,15 @@ from media import Movie
 
 # Film Coco
 coco = Movie('Coco',
-             'https://m.media-amazon.com/images/M/MV5BYjQ5NjM0Y2YtNjZkNC00ZDhkLWJjMWItN2QyNzFkMDE3ZjAxXkEyXkFqcGdeQXVyODIxMzk5NjA@._V1_UY268_CR3,0,182,268_AL_.jpg',
+             'https://m.media-amazon.com/images/M'
+             '/MV5BYjQ5NjM0Y2YtNjZkNC00ZDhkLWJjMWItN2QyNzFkMDE3ZjAxXkEyXkFqcGdeQXVyODIxMzk5NjA@._V1_UY268_CR3,0,182,'
+             '268_AL_.jpg',
              'https://www.youtube.com/watch?v=zNCz4mQzfEI')
 
 # Film The Shawshank Redemption
 shawn = Movie('The Shawshank Redemption',
-              'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg',
+              'https://m.media-amazon.com/images/M'
+              '/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg',
               'https://www.youtube.com/watch?v=6hB3S9bIaco')
 
 # Film list
